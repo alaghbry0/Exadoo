965e7ff276806c868db28c33a66475e8bb2fd31f
Mohammed Alaghbry <mohammedalaghbry3@gmail.com>
2025-03-04 07:35:18 +0300

    Fix package.json to solve Vercel deployment issue

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ async def add_security_headers(response):
         "default-src 'self'; "
         "script-src 'self' 'unsafe-inline' https://accounts.google.com; "
         "frame-src 'self' https://accounts.google.com; "
-        "connect-src 'self' https://accounts.google.com https://api.github.com https://api.nepcha.com http://localhost:5000;"
+        "connect-src 'self' https://accounts.google.com https://api.github.com https://api.nepcha.com http://localhost:5000; https://exaadoooo-p.vercel.app/"
     )
     return response
 
