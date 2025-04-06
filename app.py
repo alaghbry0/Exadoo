0012a91c8d122a965e5a1cc52c5f7017ca973786
Mohammed Alaghbry <mohammedalaghbry3@gmail.com>
2025-04-06 11:04:34 +0300

    Fix package.json to solve Vercel deployment issue

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from routes.admin_routes import admin_routes
 from routes.notifications_routes import notifications_bp
 from routes.subscriptions_routs import public_routes
 from routes.telegram_payments import payment_bp
-from server.ws_routes import ws_bp
+from routes.ws_routes import ws_bp
 
 from routes.payment_confirmation import payment_confirmation_bp
 from routes.auth_routes import auth_routes
