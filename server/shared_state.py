1c3688542a5dce0df7ffd2b790a551b9358805a4
Mohammed Alaghbry <mohammedalaghbry3@gmail.com>
2025-04-06 14:03:24 +0300

    Fix package.json to solve Vercel deployment issue

--- server/shared_state.py
+++ server/shared_state.py
@@ -1,23 +1,23 @@
 # shared_state.py
 from typing import Dict, List
-from quart.websocket import WebSocket
+from quart import websocket  # تغيير هنا
 
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: Dict[str, List[WebSocket]] = {}
+        self.active_connections: Dict[str, List[websocket.WebSocket]] = {}  # تعديل هنا
 
-    async def connect(self, telegram_id: str, websocket: WebSocket):
+    async def connect(self, telegram_id: str, ws: websocket.WebSocket):  # تعديل هنا
         if telegram_id not in self.active_connections:
             self.active_connections[telegram_id] = []
-        self.active_connections[telegram_id].append(websocket)
+        self.active_connections[telegram_id].append(ws)
 
-    def disconnect(self, telegram_id: str, websocket: WebSocket):
+    def disconnect(self, telegram_id: str, ws: websocket.WebSocket):  # تعديل هنا
         if telegram_id in self.active_connections:
-            self.active_connections[telegram_id].remove(websocket)
+            self.active_connections[telegram_id].remove(ws)
             if not self.active_connections[telegram_id]:
                 del self.active_connections[telegram_id]
 
-    def get_connections(self, telegram_id: str) -> List[WebSocket]:
+    def get_connections(self, telegram_id: str) -> List[websocket.WebSocket]:  # تعديل هنا
         return self.active_connections.get(telegram_id, [])
 
 connection_manager = ConnectionManager()
