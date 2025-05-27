4488d4bf056d66bf7cb81be81ea5e992b15dbcad
Mohammed Alaghbry <mohammedalaghbry3@gmail.com>
2025-05-27 19:34:06 +0300

    Fix package.json to solve Vercel deployment issue

--- app.py
+++ app.py
@@ -76,7 +76,9 @@ async def handle_cors(response):
     response.headers['Access-Control-Allow-Origin'] = origin
     response.headers['Access-Control-Allow-Credentials'] = 'true'
     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type, X-Requested-With'
+    response.headers['Access-Control-Allow-Headers'] = (
+        'Authorization, Content-Type, X-Requested-With, X-Telegram-Id'
+    )
     response.headers['Vary'] = 'Origin'
     response.headers['Access-Control-Max-Age'] = '86400'  # 24 ساعة
 
